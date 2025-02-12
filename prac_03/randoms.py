e650821a40049637ea1fe466ebd3cbdb47538971
Atharva <atharva.upadhyay@my.jcu.edu.au>
2025-02-12 21:05:48 +0800

    random number generation

---
+++ prac_03/randoms.py
@@ -0,0 +1,27 @@
+"""
+What did you see on line 1?
+Answer: it imported random number which are 14, 5, 4
+
+What was the smallest number you could have seen, what was the largest?
+Answer: 14 is the largest and 4 is the smallest
+
+What did you see on line 2?
+Answer: it imported random number which are 14, 5, 4
+
+What was the smallest number you could have seen, what was the largest?
+Answer: 7 is the largest and 4.6 is the smallest
+
+Could line 2 have produced a 4?
+Answer: Yes
+
+What did you see on line 3?
+Answer: it imported random number which are 14, 5, 4
+
+What was the smallest number you could have seen, what was the largest?
+Answer: 7 is the largest and 3.2 is the smallest
+"""
+
+import random
+MAXIMUM = 101
+MINIMUM = 1
+print(random.randint(MINIMUM, MAXIMUM))
