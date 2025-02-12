4b79e7d2ff6be2456cab52b858638cae9cd5624c
Atharva <atharva.upadhyay@my.jcu.edu.au>
2025-02-12 21:15:08 +0800

    password validation logic and improve input handling

---
+++ prac_03/password_checker.py
@@ -0,0 +1,57 @@
+"""
+CP1404/CP5632 - Practical
+Password checker "skeleton" code to help you get started
+"""
+
+MIN_LENGTH = 5
+MAX_LENGTH = 15
+SPECIAL_CHARS_REQUIRED = True
+SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
+
+
+def main():
+    print("Please enter a valid password")
+    print(f"Your password must be between {MIN_LENGTH} and {MAX_LENGTH} characters, and contain:")
+    print("\t1 or more uppercase characters")
+    print("\t1 or more lowercase characters")
+    print("\t1 or more numbers")
+    if SPECIAL_CHARS_REQUIRED:
+        print(f"\tand 1 or more special characters: {SPECIAL_CHARACTERS}")
+
+    password = input("> ")
+    while not is_valid_password(password):
+        print("Invalid password!")
+        password = input("> ")
+    print(f"Your {len(password)} character password is valid: {password}")
+
+
+def is_valid_password(password):
+    """Determine if the provided password is valid."""
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
+
+    has_digit = False
+    has_lower = False
+    has_upper = False
+    has_special = False
+
+    for char in password:
+        if char.isdigit():
+            has_digit = True
+        elif char.islower():
+            has_lower = True
+        elif char.isupper():
+            has_upper = True
+        elif char in SPECIAL_CHARACTERS:
+            has_special = True
+
+    # Make the final decision
+    if not (has_digit and has_lower and has_upper):
+        return False
+    if SPECIAL_CHARS_REQUIRED and not has_special:
+        return False
+
+    return True
+
+
+main()
